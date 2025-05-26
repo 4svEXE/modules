9be701fe3adeb2889be1d05a5099448c0fbdd398
Albus-489 <smartoctopusz@gmail.com>
2025-05-26 15:53:03 +0300

    fix: language switcher

--- src/app/core/components/language-switcher/language-switcher.component.ts
+++ src/app/core/components/language-switcher/language-switcher.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { LanguageService } from '../../services/language.service';
+import { NgClass } from '@angular/common';
 
 @Component({
+  imports: [NgClass],
   selector: 'app-language-switcher',
   templateUrl: './language-switcher.component.html',
   styleUrls: ['./language-switcher.component.scss']
 })
 export class LanguageSwitcherComponent {
+  selectedLang: string;
 
-  constructor(private languageService: LanguageService) { }
+  constructor(private languageService: LanguageService) {
+    this.selectedLang = this.languageService.getLanguage();
+  }
 
   // Метод для зміни мови
   changeLanguage(language: string): void {
     this.languageService.setLanguage(language);
+    this.selectedLang = language;
   }
 }
--- src/app/pages/home/hero-section/hero-section.component.ts
+++ src/app/pages/home/hero-section/hero-section.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MarqueeComponent } from "../../../shared/components/marquee/marquee.component";
 import { CallToActionComponent } from "../../../shared/components/call-to-action/call-to-action.component";
 import { TranslateModule } from '@ngx-translate/core';
+import { LanguageSwitcherComponent } from "../../../core/components/language-switcher/language-switcher.component";
 
 @Component({
   selector: 'app-hero-section',
-  imports: [MarqueeComponent, CallToActionComponent, TranslateModule],
+  imports: [MarqueeComponent, CallToActionComponent, TranslateModule, LanguageSwitcherComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
